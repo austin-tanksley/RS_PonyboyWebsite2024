60380b048a39cd63caf39e90b745e8d00e641e73
Austin <58612782+austin-tanksley@users.noreply.github.com>
2024-12-03 22:28:06 -0800

    production build 1.1.0 - added animation

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
-import "./flickity.pkgd.min.js"
 
-console.log('Main entry point');
+import "./flickity.pkgd.min.js"
+import { animate } from "https://cdn.jsdelivr.net/npm/motion@11.11.13/+esm"
+
+animate('.hero__video', {y: [300,0]},
+    {duration: 1, ease: [0.39, 0.24, 0.3, 1]}
+
+)
+
+
