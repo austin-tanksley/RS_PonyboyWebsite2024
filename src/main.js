6dac1966fee07e5074b471fbff5cee0db13d67ac
Austin <58612782+austin-tanksley@users.noreply.github.com>
2024-12-03 22:47:33 -0800

    production build 1.1.2 - added animation

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { animate } from "https://cdn.jsdelivr.net/npm/motion@11.11.13/+esm"
 // animate('.hero__video', {y: [300,0]},
 //     {duration: 1, ease: [0.39, 0.24, 0.3, 1]}
 // )
-animate('.hero__video', {y: [400, 0], opacity: [0, 1]},
+animate('.hero__video', {opacity: [0, 1]},
     {duration: 1, ease: [0.39, 0.24, 0.3, 1]}
 )
 
